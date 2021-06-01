f44a74e8dad9cc75565ab6ca286988de93809d62
Pierre Rudloff <prudloff@insite.coop>
2021-06-01 10:44:41 +0200

    Méthode dépréciée (Insite #26862)

--- ArchiFooter.php
+++ ArchiFooter.php
@@ -80,7 +80,7 @@ class ArchiFooter
         global $wgUser, $wgParser;
         $title = $skin->getTitle();
         $article = new Article($title);
-        if ($article->getID() > 0 && in_array($title->getNamespace(), [NS_ADDRESS, NS_ADDRESS_NEWS, NS_PERSON])) {
+        if ($article->getPage()->getId() > 0 && in_array($title->getNamespace(), [NS_ADDRESS, NS_ADDRESS_NEWS, NS_PERSON])) {
             //Edit button
             $return .= '<p>'. Html::rawElement(
                 'a',
